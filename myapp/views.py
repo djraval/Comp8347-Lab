37ef73be7bfdc84be16293d8acbf77730dce5fbe
djraval <devarshi.j.raval@gmail.com>
2020-07-12 16:07:07 -0400

    Lab 6 : Part A

--- myapp/views.py
+++ myapp/views.py
@@ -1,43 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Topic, Course
 
 
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    response = HttpResponse()
-    heading1 = '<p>' + 'List of topics: ' + '</p>'
-    response.write(heading1)
-    for topic in top_list:
-        para = '<p>' + str(topic.id) + ': ' + str(topic) + '</p>'
-        response.write(para)
-
-    response.write('<p>' + 'List of courses: ' + '</p>')
-    for course in Course.objects.all().order_by('-price')[:5]:
-        if course.for_everyone:
-            for_str = "This Course is For Everyone!"
-        else:
-            for_str = "This Course is not For Everyone!"
-        response.write('<p>' + str(course.id) + ': ' + str(course) + for_str + '</p>')
-    return response
+    return render(request, "myapp/index0.html", {'top_list': top_list})
 
 
 def about(request):
-    return HttpResponse("This is an E-learning Website! Search our Topics to find all available Courses.")
+    # return HttpResponse("This is an E-learning Website! Search our Topics to find all available Courses.")
+    return render(request, "myapp/about0.html")
 
 
 def detail(request, top_no):
-    response = HttpResponse()
-    #topic  = Topic.objects.get(id=top_no)          #Standard answer
-    topic = get_object_or_404(Topic, id=top_no)     #Q4 Gives 404 if Topic not found
-    response.write(topic)
-
-    response.write('<p>' + 'List of courses: ' + '</p>')
-    for course in Course.objects.filter(topic__id=top_no):
-        if course.for_everyone:
-            for_str = "This Course is For Everyone!"
-        else:
-            for_str = "This Course is not For Everyone!"
-        response.write('<p>' + str(course.id) + ': ' + str(course) + for_str + '</p>')
-    return response
+    #response = HttpResponse()
+    # topic  = Topic.objects.get(id=top_no)          #Standard answer
+    topic = get_object_or_404(Topic, id=top_no)  # Q4 Gives 404 if Topic not found
+    course_list = Course.objects.filter(topic__id=top_no)
+    # response.write(topic)
+    # response.write('<p>' + 'List of courses: ' + '</p>')
+    # for course in Course.objects.filter(topic__id=top_no):
+    #     if course.for_everyone:
+    #         for_str = "This Course is For Everyone!"
+    #     else:
+    #         for_str = "This Course is not For Everyone!"
+    #     response.write('<p>' + str(course.id) + ': ' + str(course) + for_str + '</p>')
+    # return response
+    return render(request,"myapp/detail0.html",{'topic':topic, 'course_list': course_list})
