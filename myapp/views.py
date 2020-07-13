ff1517086910e5741bc14b7f956b68b97151b4a9
djraval <devarshi.j.raval@gmail.com>
2020-07-12 22:01:02 -0400

    Lab 6 : Part B

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Topic, Course
@@ -6,16 +5,16 @@ from .models import Topic, Course
 
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    return render(request, "myapp/index0.html", {'top_list': top_list})
+    return render(request, "myapp/index.html", {'top_list': top_list})
 
 
 def about(request):
     # return HttpResponse("This is an E-learning Website! Search our Topics to find all available Courses.")
-    return render(request, "myapp/about0.html")
+    return render(request, "myapp/about.html")
 
 
 def detail(request, top_no):
-    #response = HttpResponse()
+    # response = HttpResponse()
     # topic  = Topic.objects.get(id=top_no)          #Standard answer
     topic = get_object_or_404(Topic, id=top_no)  # Q4 Gives 404 if Topic not found
     course_list = Course.objects.filter(topic__id=top_no)
@@ -28,4 +27,4 @@ def detail(request, top_no):
     #         for_str = "This Course is not For Everyone!"
     #     response.write('<p>' + str(course.id) + ': ' + str(course) + for_str + '</p>')
     # return response
-    return render(request,"myapp/detail0.html",{'topic':topic, 'course_list': course_list})
+    return render(request, "myapp/detail.html", {'topic': topic, 'course_list': course_list})
